e31950c6894973c14c58dfed702f195871f374ba
Matt Kuda <mattkuda@gmail.com>
2024-11-26 21:02:57 -0500

    Remvoe muted bg on about section

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import Link from "next/link"
 const AboutSection = () => {
     return (
-        <section className="bg-muted py-12">
+        <section className="py-12">
             <div className="container mx-auto px-4 text-center">
                 <h3 className="text-3xl font-bold mb-4">About Me</h3>
                 <p className="text-xl mb-6">
